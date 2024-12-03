73a6ff8bdc8e77fff28bde411866d656d45f580c
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-03 15:03:33 +0100

    main page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,74 @@
 <script setup>
   import { RouterLink, RouterView } from "vue-router";
+  import logo from "./assets/logo.svg";
 </script>
 
 <template>
   <header>
+    <div class="page-name">
+      <img :src="logo" alt="logo" />
+      <p>Advent CSS 2024</p>
+    </div>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <RouterLink to="/about" class="nav-link">About</RouterLink>
     </nav>
   </header>
 
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+  $accent-color: #30aa82;
+
+  header {
+    position: relative;
+    // padding: 15px;
+
+    border-bottom: 2px solid $accent-color;
+
+    // background-color: aqua;
+  }
+
+  .page-name {
+    position: absolute;
+    left: 20px;
+    top: 0;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    height: 100%;
+
+    img {
+      width: 30px;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
+  }
+
+  nav {
+    display: flex;
+    justify-content: center;
+
+    width: 100%;
+  }
+
+  .nav-link {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    height: 50px;
+    padding: 10px 20px;
+
+    font-weight: bold;
+    font-size: 1.2rem;
+    text-decoration: none;
+
+    &:hover {
+      background-color: $accent-color;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,69 @@
 <script setup>
   import { RouterLink } from "vue-router";
+  import galleryPhoto from "../assets/images/challenge1/photo1.jpg";
 </script>
 
 <template>
-  <div>Hello</div>
-  <RouterLink to="/challenge1">Challenge1</RouterLink>
+  <div class="container">
+    <RouterLink to="/challenge1" class="project-link">
+      <img :src="galleryPhoto" alt="Challenge 1" />
+      <p>1 - Gallery</p>
+    </RouterLink>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @layer components {
+    .container {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+      align-items: center;
+      gap: 1rem;
+
+      height: 100%;
+      padding: 10px 30px;
+    }
+
+    .project-link {
+      position: relative;
+      // max-width: 240px;
+      // height: 150px;
+
+      overflow: hidden;
+      border-radius: 0.5rem;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transition: all 0.2s ease-in-out;
+      }
+
+      p {
+        position: absolute;
+        bottom: -30%;
+        width: 100%;
+        padding: 5px 10px;
+        background-color: rgba(0, 0, 0, 0.5);
+
+        transition: all 0.2s ease-in-out;
+      }
+
+      &:hover {
+        img {
+          transform: scale(1.1);
+        }
+        p {
+          bottom: 0;
+        }
+      }
+    }
+
+    @media (min-width: 1024px) {
+      .project-link {
+        max-width: 300px;
+        height: 180px;
+      }
+    }
+  }
+</style>
