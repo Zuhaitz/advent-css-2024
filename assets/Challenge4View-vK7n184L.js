83227625be6eece8328aaff04facbac1a3ebf6ba
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-08 14:12:54 +0100

    Updates

---
+++ assets/Challenge4View-vK7n184L.js
@@ -0,0 +1,1 @@
+import{_ as n,c as a,b as s,o}from"./index-DX-4sR_i.js";const t={},c={class:"container"};function r(_,e){return o(),a("section",c,e[0]||(e[0]=[s("h1",{class:"heading"},[s("span",null,"Heading")],-1)]))}const l=n(t,[["render",r],["__scopeId","data-v-81f46d76"]]);export{l as default};
