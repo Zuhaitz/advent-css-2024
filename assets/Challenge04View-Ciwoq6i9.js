5ce739a9a9ea72651772a84afb7c62aa6f54cf70
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-19 09:38:03 +0100

    Updates

--- assets/Challenge04View-Ciwoq6i9.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as a,b as s,o}from"./index-iLGXohus.js";const t={},c={class:"container"};function r(_,e){return o(),a("section",c,e[0]||(e[0]=[s("h1",{class:"heading"},[s("span",null,"Heading")],-1)]))}const l=n(t,[["render",r],["__scopeId","data-v-2deb7018"]]);export{l as default};
