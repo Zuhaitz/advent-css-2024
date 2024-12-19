7d7c5a41a290d8d17b97fbe805c8ad6722317edd
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-19 09:28:30 +0100

    Updates

--- assets/Challenge09View-BtfpcL_U.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,b as e,o as n}from"./index-Dl0V860l.js";const o={},c={class:"container"};function r(d,t){return n(),a("section",c,t[0]||(t[0]=[e("h1",{class:"page_title"},"9 - Button with a Gradient Border",-1),e("button",{class:"gradient-button"},[e("span",null,"CSS Rocks!")],-1)]))}const _=s(o,[["render",r],["__scopeId","data-v-87adee4d"]]);export{_ as default};
