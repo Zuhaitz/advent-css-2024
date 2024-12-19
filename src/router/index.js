97d1da0db25253559f7e1b869a94569f2642a036
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-19 09:35:39 +0100

    change web history

--- src/router/index.js
+++ src/router/index.js
@@ -15,222 +15,231 @@ const router = createRouter({
   scrollBehavior() {
     return { top: 0 };
   },
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "default",
-      component: DefaultLayout,
-      redirect: "/",
+      path: import.meta.env.BASE_URL,
       children: [
         {
           path: "/",
-          name: "home",
-          component: HomeView,
-          meta: {
-            enterClass: "animate__animated animate__fadeInLeft",
-            leaveClass: "animate__animated animate__fadeOutRight",
-          },
-        },
-        {
-          path: "/about",
-          name: "about",
-          component: () => import("../views/AboutView.vue"),
-          meta: {
-            enterClass: "animate__animated animate__fadeInRight",
-            leaveClass: "animate__animated animate__fadeOutLeft",
-          },
-        },
-        {
-          path: "/challenge1",
-          name: "challenge1",
-          component: () => import("../views/Challenge01View.vue"),
-          meta: {
-            next: { path: "/challenge2", title: "2 - CSS Only Toggle" },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge2",
-          name: "challenge2",
-          component: () => import("../views/Challenge02View.vue"),
-          meta: {
-            previous: { path: "/challenge1", title: "1 - Gallery" },
-            next: { path: "/challenge3", title: "3 - CSS Only Tooltip" },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge3",
-          name: "challenge3",
-          component: () => import("../views/Challenge03View.vue"),
-          meta: {
-            previous: { path: "/challenge2", title: "2 - CSS Only Toggle" },
-            next: { path: "/challenge4", title: "4 - Header Styling" },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge4",
-          name: "challenge4",
-          component: () => import("../views/Challenge04View.vue"),
-          meta: {
-            previous: { path: "/challenge3", title: "3 - CSS Only Tooltip" },
-            next: {
+          name: "default",
+          component: DefaultLayout,
+          redirect: "/",
+          children: [
+            {
+              path: "/",
+              name: "home",
+              component: HomeView,
+              meta: {
+                enterClass: "animate__animated animate__fadeInLeft",
+                leaveClass: "animate__animated animate__fadeOutRight",
+              },
+            },
+            {
+              path: "/about",
+              name: "about",
+              component: () => import("../views/AboutView.vue"),
+              meta: {
+                enterClass: "animate__animated animate__fadeInRight",
+                leaveClass: "animate__animated animate__fadeOutLeft",
+              },
+            },
+            {
+              path: "/challenge1",
+              name: "challenge1",
+              component: () => import("../views/Challenge01View.vue"),
+              meta: {
+                next: { path: "/challenge2", title: "2 - CSS Only Toggle" },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge2",
+              name: "challenge2",
+              component: () => import("../views/Challenge02View.vue"),
+              meta: {
+                previous: { path: "/challenge1", title: "1 - Gallery" },
+                next: { path: "/challenge3", title: "3 - CSS Only Tooltip" },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge3",
+              name: "challenge3",
+              component: () => import("../views/Challenge03View.vue"),
+              meta: {
+                previous: { path: "/challenge2", title: "2 - CSS Only Toggle" },
+                next: { path: "/challenge4", title: "4 - Header Styling" },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge4",
+              name: "challenge4",
+              component: () => import("../views/Challenge04View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge3",
+                  title: "3 - CSS Only Tooltip",
+                },
+                next: {
+                  path: "/challenge5",
+                  title: "5 - Scroll up to Reveal the Footer",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
               path: "/challenge5",
-              title: "5 - Scroll up to Reveal the Footer",
+              name: "challenge5",
+              component: () => import("../views/Challenge05View.vue"),
+              meta: {
+                previous: { path: "/challenge4", title: "4 - Header Styling" },
+                next: {
+                  path: "/challenge6",
+                  title: "6 - Scroll Indicator",
+                },
+                ...challengesAnim,
+              },
             },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge5",
-          name: "challenge5",
-          component: () => import("../views/Challenge05View.vue"),
-          meta: {
-            previous: { path: "/challenge4", title: "4 - Header Styling" },
-            next: {
+            {
               path: "/challenge6",
-              title: "6 - Scroll Indicator",
+              name: "challenge6",
+              component: () => import("../views/Challenge06View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge5",
+                  title: "5 - Scroll up to Reveal the Footer",
+                },
+                next: {
+                  path: "/challenge7",
+                  title: "7 - Animated Label",
+                },
+                ...challengesAnim,
+              },
             },
-            ...challengesAnim,
-          },
+            {
+              path: "/challenge7",
+              name: "challenge7",
+              component: () => import("../views/Challenge07View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge6",
+                  title: "6 - Scroll Indicator",
+                },
+                next: {
+                  path: "/challenge8",
+                  title: "8 - Scroll into View, Top Nav",
+                },
+                ...challengesAnim,
+              },
+            },
+
+            {
+              path: "/challenge9",
+              name: "challenge9",
+              component: () => import("../views/Challenge09View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge8",
+                  title: "8 - Scroll into View, Top Nav",
+                },
+                next: {
+                  path: "/challenge10",
+                  title: "10 - Horizontal Scroller with Scroll Snap",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge10",
+              name: "challenge10",
+              component: () => import("../views/Challenge10View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge9",
+                  title: "9 - Button with a Gradient Border",
+                },
+                next: {
+                  path: "/challenge11",
+                  title: "11 - Vertical Text with Outline",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge11",
+              name: "challenge11",
+              component: () => import("../views/Challenge11View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge10",
+                  title: "10 - Horizontal Scroller with Scroll Snap",
+                },
+                next: {
+                  path: "/challenge12",
+                  title: "12 - Same HTML, Different CSS",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge12",
+              name: "challenge12",
+              component: () =>
+                import("../views/challenge12/Challenge12View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge11",
+                  title: "11 - Vertical Text with Outline",
+                },
+                next: {
+                  path: "/challenge13",
+                  title: "13 - Three-State Toggle",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/challenge13",
+              name: "challenge13",
+              component: () => import("../views/Challenge13View.vue"),
+              meta: {
+                previous: {
+                  path: "/challenge12",
+                  title: "12 - Same HTML, Different CSS",
+                },
+                next: {
+                  path: "/challenge14",
+                  title: "14 - Corner Ribbon",
+                },
+                ...challengesAnim,
+              },
+            },
+            {
+              path: "/:pathMatch(.*)*",
+              name: "not-found",
+              redirect: "/",
+            },
+          ],
         },
         {
-          path: "/challenge6",
-          name: "challenge6",
-          component: () => import("../views/Challenge06View.vue"),
+          path: "/challenge8",
+          name: "challenge8",
+          component: () => import("../views/Challenge08View.vue"),
           meta: {
             previous: {
-              path: "/challenge5",
-              title: "5 - Scroll up to Reveal the Footer",
-            },
-            next: {
               path: "/challenge7",
               title: "7 - Animated Label",
             },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge7",
-          name: "challenge7",
-          component: () => import("../views/Challenge07View.vue"),
-          meta: {
-            previous: {
-              path: "/challenge6",
-              title: "6 - Scroll Indicator",
-            },
             next: {
-              path: "/challenge8",
-              title: "8 - Scroll into View, Top Nav",
-            },
-            ...challengesAnim,
-          },
-        },
-
-        {
-          path: "/challenge9",
-          name: "challenge9",
-          component: () => import("../views/Challenge09View.vue"),
-          meta: {
-            previous: {
-              path: "/challenge8",
-              title: "8 - Scroll into View, Top Nav",
-            },
-            next: {
-              path: "/challenge10",
-              title: "10 - Horizontal Scroller with Scroll Snap",
-            },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge10",
-          name: "challenge10",
-          component: () => import("../views/Challenge10View.vue"),
-          meta: {
-            previous: {
               path: "/challenge9",
               title: "9 - Button with a Gradient Border",
             },
-            next: {
-              path: "/challenge11",
-              title: "11 - Vertical Text with Outline",
-            },
             ...challengesAnim,
           },
-        },
-        {
-          path: "/challenge11",
-          name: "challenge11",
-          component: () => import("../views/Challenge11View.vue"),
-          meta: {
-            previous: {
-              path: "/challenge10",
-              title: "10 - Horizontal Scroller with Scroll Snap",
-            },
-            next: {
-              path: "/challenge12",
-              title: "12 - Same HTML, Different CSS",
-            },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge12",
-          name: "challenge12",
-          component: () => import("../views/challenge12/Challenge12View.vue"),
-          meta: {
-            previous: {
-              path: "/challenge11",
-              title: "11 - Vertical Text with Outline",
-            },
-            next: {
-              path: "/challenge13",
-              title: "13 - Three-State Toggle",
-            },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/challenge13",
-          name: "challenge13",
-          component: () => import("../views/Challenge13View.vue"),
-          meta: {
-            previous: {
-              path: "/challenge12",
-              title: "12 - Same HTML, Different CSS",
-            },
-            next: {
-              path: "/challenge14",
-              title: "14 - Corner Ribbon",
-            },
-            ...challengesAnim,
-          },
-        },
-        {
-          path: "/:pathMatch(.*)*",
-          name: "not-found",
-          redirect: "/",
         },
       ],
-    },
-    {
-      path: "/challenge8",
-      name: "challenge8",
-      component: () => import("../views/Challenge08View.vue"),
-      meta: {
-        previous: {
-          path: "/challenge7",
-          title: "7 - Animated Label",
-        },
-        next: {
-          path: "/challenge9",
-          title: "9 - Button with a Gradient Border",
-        },
-        ...challengesAnim,
-      },
     },
   ],
 });
