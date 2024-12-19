af78fadeab21c234a00efdeb816e84754c13df50
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-19 09:17:07 +0100

    change web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 
@@ -11,7 +11,7 @@ const router = createRouter({
   scrollBehavior() {
     return { top: 0 };
   },
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
