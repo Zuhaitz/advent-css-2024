a5bbc4977e4be8a84f268068691131e88b968b7d
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-19 14:08:02 +0100

    ribbon done

--- src/views/Challenge14View.vue
+++ src/views/Challenge14View.vue
@@ -1,16 +1,35 @@
 <template>
   <section class="container">
     <h1 class="page_title">14 - Corner Ribbon</h1>
-    <div class="corner-ribbon"><p>Open source</p></div>
 
     <div class="card">
-      <div class="corner-ribbon"><p>Open source</p></div>
+      <h2>This is a card :)</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
+        error voluptatum accusamus voluptatem ad eveniet hic exercitationem
+        eaque tenetur porro eligendi libero ratione distinctio non, quisquam
+        unde aut assumenda fugit!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non minus
+        consectetur in doloribus atque laborum ratione, ex, odio officiis,
+        consequatur minima nostrum accusamus officia recusandae provident hic
+        repudiandae aliquid quibusdam.
+      </p>
+
+      <div class="corner-ribbon">
+        <p><span>Open source</span></p>
+      </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
   $dimensions: 100px;
+  $ribbon-height: 30px;
+  $font-size: 0.5rem;
+  $overflow: 10px;
   $green-light: #30aa82;
   $green-dark: #214e2e;
 
@@ -24,34 +43,106 @@
 
     .card {
       position: relative;
-      width: min(800px, 90%);
-      height: 500px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 10px;
+
+      width: min(900px, 90%);
+      // height: 500px;
+      padding: 20px 50px;
+
       border: 3px solid $green-light;
-      overflow: hidden;
+      border-radius: 10px;
+
+      h2 {
+        text-align: center;
+        font-size: 2rem;
+        text-wrap: balance;
+      }
+
+      p {
+        text-align: center;
+      }
     }
 
     .corner-ribbon {
       position: absolute;
-      top: 0;
-      right: 0;
+      top: -$overflow;
+      right: -$overflow;
+
       width: $dimensions;
       height: $dimensions;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
       overflow: hidden;
 
       p {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%) rotate(45deg);
+        flex: 0 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
-        padding: 5px;
-        width: 200%;
+        width: 150%;
+        height: $ribbon-height;
+        transform: rotate(45deg) translateY(calc($ribbon-height / -2));
 
-        font-size: 0.5rem;
         text-align: center;
         text-transform: uppercase;
+        font-weight: bold;
+        font-size: $font-size;
+        letter-spacing: 0.1rem;
 
         background-color: $green-light;
+      }
+
+      &::before,
+      &::after {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        width: $overflow;
+        height: $overflow;
+        background-color: $green-dark;
+      }
+
+      &::before {
+        top: 0;
+        left: 0;
+      }
+
+      &::after {
+        bottom: 0;
+        right: 0;
+      }
+    }
+
+    @media (min-width: 640px) {
+      $dimensions: 120px;
+      $ribbon-height: 35px;
+      $font-size: 0.8rem;
+      $overflow: 12px;
+
+      .corner-ribbon {
+        top: -$overflow;
+        right: -$overflow;
+
+        width: $dimensions;
+        height: $dimensions;
+
+        p {
+          height: $ribbon-height;
+          transform: rotate(45deg) translateY(calc($ribbon-height / -2));
+          font-size: $font-size;
+        }
+
+        &::before,
+        &::after {
+          width: $overflow;
+          height: $overflow;
+        }
       }
     }
   }
