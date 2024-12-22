7d5829a2a7c8038dbfe0d4a09ef559381f108f6c
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-22 14:10:35 +0100

    Updates

---
+++ assets/Challenge09View-CMh8vKDY.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,b as e,o as n}from"./index-CVbVDZen.js";const o={},c={class:"container"};function r(l,t){return n(),a("section",c,t[0]||(t[0]=[e("h1",{class:"page-title"},"9 - Button with a Gradient Border",-1),e("button",{class:"gradient-button"},[e("span",null,"CSS Rocks!")],-1)]))}const i=s(o,[["render",r],["__scopeId","data-v-0b300d6a"]]);export{i as default};
