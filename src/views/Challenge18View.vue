3332d3ec2972b1a1793e063e2dc2a17e85c6b319
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-24 10:50:44 +0100

    day 18 done

--- src/views/Challenge18View.vue
+++ src/views/Challenge18View.vue
@@ -2,20 +2,73 @@
   <section class="container">
     <h1 class="page-title">18 - Skeleton Loading Image</h1>
 
-    <div class="skeleton">
-      <div class="skeleton__avatar"></div>
-      <div class="skeleton__data">
-        <p></p>
-        <p></p>
+    <div class="flex-center">
+      <div class="skeleton blink">
+        <div class="skeleton__avatar"></div>
+        <div class="skeleton__data">
+          <p></p>
+          <p></p>
+        </div>
+
+        <div class="skeleton__content"></div>
       </div>
 
-      <div class="skeleton__content"></div>
+      <div class="skeleton shimmer">
+        <div class="skeleton__avatar"></div>
+        <div class="skeleton__data">
+          <p></p>
+          <p></p>
+        </div>
+
+        <div class="skeleton__content"></div>
+      </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+  $background-color: #d9d9d9;
+
   @layer components {
+    @keyframes blink-animation {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0.4;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+
+    @keyframes shimmer-animation {
+      to {
+        background-position-x: -50%;
+      }
+    }
+
+    .blink > div {
+      animation: blink-animation 3s infinite linear;
+    }
+
+    .shimmer {
+      .skeleton__avatar,
+      .skeleton__data > p,
+      .skeleton__content {
+        background: linear-gradient(
+          to right,
+          $background-color 40%,
+          white 50%,
+          $background-color 60%
+        );
+        background-size: 300%;
+        background-position-x: 100%;
+
+        animation: shimmer-animation 3s infinite linear;
+      }
+    }
+
     .container {
       display: flex;
       flex-direction: column;
@@ -39,7 +92,7 @@
       &__avatar {
         width: 124px;
         min-height: 124px;
-        background-color: #d9d9d9;
+        background-color: $background-color;
         border-radius: 100%;
 
         grid-area: avatar;
@@ -56,7 +109,7 @@
         p {
           height: 25px;
           border-radius: 6px;
-          background-color: #d9d9d9;
+          background-color: $background-color;
 
           &:first-child {
             height: 55px;
@@ -73,7 +126,7 @@
         height: 100%;
         flex-grow: 1;
         border-radius: 10px;
-        background-color: #d9d9d9;
+        background-color: $background-color;
 
         grid-area: content;
       }
